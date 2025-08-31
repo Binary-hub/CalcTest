ec104c738eb1480312edc98b50d09a3f40a89534
Nithin <48122794+Binary-hub@users.noreply.github.com>
2025-08-31 23:07:48 +0530

    Update Calculator.java mul method
    mul method updated

--- Calculator.java
+++ Calculator.java
@@ -21,11 +21,12 @@ public class Calculator {
 	   Calculator cal = new Calculator();
 	   System.out.println("Addition: " + cal.add(10, 5));
        System.out.println("Subtraction: " + cal.sub(10, 5));
-	   System.out.println("Multiplication: " + cal.mul(10, 5));
+	   System.out.println("Multiplication1: " + cal.mul(10, 5));
 	   //System.out.println("Division: " + cal.div(10, 5));
 	}
 
 
 }
+
 
 
